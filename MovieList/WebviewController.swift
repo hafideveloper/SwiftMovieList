0de9a3345f090f9b55bebff8f3ad69d7f9a9493d
hafideveloper <hafideveloper@gmail.com>
2024-08-22 11:36:13 +0300

    WebKit added

---
+++ MovieList/WebviewController.swift
@@ -0,0 +1,18 @@
+//
+//  WebviewController.swift
+//  MovieList
+//
+//  Created by Apple on 22.08.2024.
+//
+
+import Foundation
+import UIKit
+import WebKit
+
+class WebviewController:UIViewController {
+    @IBOutlet var webView:WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
